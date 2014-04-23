e8335fabcbf9135e88930174ecf51e5db8d889a6
Ethanol0316 <ethanol0316@gmail.com>
2014-04-23 10:32:59 -0400

    Added new method of capturing NFC intents.

--- MODSApp/src/edu/fau/mods/modsapp/MainMenu.java
+++ MODSApp/src/edu/fau/mods/modsapp/MainMenu.java
@@ -3,15 +3,22 @@ package edu.fau.mods.modsapp;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenu extends Activity {
 
+	public String nfcData;
+	
 	Context context = this;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,5 +45,18 @@ public class MainMenu extends Activity {
 			}
 			
 		});
-	}
+		
+		Intent intent = getIntent();
+	    if(intent.getAction().equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+	        // Read the first record which contains the NFC data
+	        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+	        NdefRecord record = ((NdefMessage)rawMsgs[0]).getRecords()[0];
+	        nfcData = new String(record.getPayload());
+
+	        // Display the data on the tag
+	        Toast.makeText(this, nfcData, Toast.LENGTH_LONG).show();
+	 
+	        // Do other stuff with the data...
+		}
+	}	
 }
