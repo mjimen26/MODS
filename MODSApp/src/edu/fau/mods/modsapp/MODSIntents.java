92ac9a5b370479fb50a535a59b2aeb8185584690
Marvin <fadestaynight@gmail.com>
2014-04-30 17:39:43 -0400

    Added new files

---
+++ MODSApp/src/edu/fau/mods/modsapp/MODSIntents.java
@@ -0,0 +1,16 @@
+package edu.fau.mods.modsapp;
+
+public class MODSIntents {
+
+	public static final String AQUARIUM = "mods.ecoscapes.AQUARIUM_INTENT";
+	public static final String SHARK = "mods.ecoscapes.SHARK_INTENT";
+	public static final String LOBSTER = "mods.ecoscapes.LOBSTER_INTENT";
+	public static final String LIVING = "mods.ecoscapes.LIVING_INTENT";
+	public static final String POISON = "mods.ecoscapes.POISON_INTENT";
+	public static final String ARTIFICIAL = "mods.ecoscapes.ARTIFICIAL_INTENT";
+	public static final String BLIND = "mods.ecoscapes.BLIND_INTENT";
+	public static final String SCHOOL = "mods.ecoscapes.SCHOOL_INTENT";
+	
+	public static final String HORSESHOE = "mods.ecoscapes.HORSESHOE_INTENT";
+	
+}
